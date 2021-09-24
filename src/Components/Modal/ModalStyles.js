59277bb53c5ec40e6e10a61c6b86fa3810f98edd
MarkGee45 <moel55.mg@gmail.com>
2021-09-24 12:19:50 -0700

    clean up comments

--- src/Components/Modal/ModalStyles.js
+++ src/Components/Modal/ModalStyles.js
@@ -8,7 +8,6 @@ export const Container = styled.div`
 `;
 
 export const ModalT = styled.div`
-
     width: 614px;
     height: 558px;
     background-color: rgba(0, 0, 0, .8);
@@ -19,38 +18,11 @@ export const ModalT = styled.div`
     align-items: center;
     justify-content: space-around;
     border-radius: 15px;
-    /* @media(min-width: 1024px){
-    }
-    @media(min-width: 768px){
-        width: 500px; 
-        height: 460px; 
-        background-color: rgba(0, 0, 0, .8);
-        z-index: 10;
-        position: fixed;
-        top: 18%;
-        display: flex; 
-        align-items: center;
-        justify-content: space-around;
-        border-radius: 15px;
-    } */
-   /* @media(min-width: 425px){
-        width: 25%;  
-    } */
 `;
 
 export const Poster = styled.div`
-
     height: 465px;
     width: 285px;
-    /* @media(min-width: 1024px){
-    }
-    @media(min-width: 768px){
-        width: 200px; 
-        height: 400px; 
-    } */
-    /*@media(min-width: 425px){
-        width: 25%;  
-    } */
 `;
 
 export const Content = styled.div`
@@ -58,58 +30,23 @@ export const Content = styled.div`
     width: 246px;
     height: 465px;  
     
-    /* @media(min-width: 1024px){ 
-    }
-    @media(min-width: 768px){
-        width: 200px; 
-        height: 400px; 
-    } */
-   /* @media(min-width: 425px){
-        width: 25%;  
-    } */
 `;
 
 export const Title = styled.h4`
     color: rgb(217, 217, 217);
     font-size: 15px;  
     
-    /* @media(min-width: 1024px){
-    }
-    @media(min-width: 768px){
-        font-size: 12px;  
-    } */
-    /*@media(min-width: 425px){
-        font-size: 10px;  
-    } */
+    
 `;
 
 export const Info = styled.h4`
     color: rgb(217, 217, 217);
     font-size: 12px;  
     
-    /* @media(min-width: 1024px){
-    }
-    @media(min-width: 768px){
-        font-size: 9px;  
-    } */
-    /*@media(min-width: 425px){
-        font-size: 7px;  
-    } */
     `;
 
 export const Img = styled.img`
     
-    /* @media(min-width: 1024px){
-        height: 402px;
-        width: 284;  
-    }
-    @media(min-width: 768px){
-        height: 350px;
-        width: 204px;  
-    } */
-    /*@media(min-width: 425px){
-        height: 300px;  
-    } */
 `;
 
 export const Button = styled.button`
@@ -121,15 +58,6 @@ export const Button = styled.button`
     font-size: 15px;
     cursor: pointer;
     
-    /* @media(min-width: 1024px){  
-    }
-    @media(min-width: 768px){
-        background-color: red;
-        height: 25px;
-        padding: 5px;
-        font-weight: 600;
-        font-size: 12px;  
-    }  */
 `;
 export const Blur = styled.div`
     backdrop-filter: blur( 4px );
@@ -143,27 +71,5 @@ export const Blur = styled.div`
     width: 100%;
     display: flex; 
     align-items: center;
-    /* justify-content: space-evenly; */
-    /* border: 3px solid rgba(140, 140, 140, 0.18); */
-    /* background-color:rgba(0, 0, 0, .8);  */
-    /* border-radius: 15px;  */
-    /* box-shadow: 9px 3px 18px 0 rgba( 77, 77, 77, 0.37 ), -9px -3px 18px 0 rgba( 77, 77, 77, 0.37 ); */
-    /* box-shadow: 9px 3px 18px 0 rgba( 138, 138, 138, 0.37 ), -9px -3px 18px 0 rgba( 138, 138, 138, 0.37 ); */
-    /* @media(min-width: 1024px){ */
-        
-         
-    /* } */
-    /* @media(min-width: 768px){
-        top: 10vh;
-        width: 700px;
-        height: 500px;
-        margin:9px 0;  
-    }
-    @media(min-width: 425px){
-        top: 10vh;
-        width: 700px;
-        height: 500px;
-        margin:5px 0;  
-    } */
     
 `;
